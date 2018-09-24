ffed6cc4c6281a827007f9e6d98ec76b0704826b
rtalukder <rtalukder@forsythe.com>
2018-09-24 19:01:27 -0400

    week3 and final project code added.

---
+++ week3/hw2/src/findOrderStrategy/FindOrderStrategyFactory.java
@@ -0,0 +1,15 @@
+package findOrderStrategy;
+
+public class FindOrderStrategyFactory {
+    public static IFindOrderStrategy getFindByRewardsNumberStrategy() {
+        throw new Error("Not yet implemented");
+    }
+
+    public static IFindOrderStrategy getFindByLastNameStrategy() {
+        throw new Error("Not yet implemented");
+    }
+
+    public static IFindOrderStrategy getFindByOrderNumberStrategy() {
+        throw new Error("Not yet implemented");
+    }
+}
---
+++ week3/hw2/src/pizza/PizzaBuilder.java
@@ -0,0 +1,19 @@
+package pizza;
+
+public class PizzaBuilder implements IPizzaBuilder {
+
+    @Override
+    public void addTopping(String topping) {
+        throw new Error("Not yet implemented");
+    }
+
+    @Override
+    public void setSize(String size) {
+        throw new Error("Not yet implemented");
+    }
+
+    @Override
+    public Pizza getPizza() {
+        throw new Error("Not yet implemented");
+    }
+}
